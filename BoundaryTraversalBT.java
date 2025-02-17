22e0e9923d7da2aff7301bcb93aa749efc47baf9
Aditya Pandey <2811adityapandey@gmail.com>
2025-02-17 19:44:32 +0530

    Update BoundaryTraversalBT.java

--- BoundaryTraversalBT.java
+++ BoundaryTraversalBT.java
@@ -1,3 +1,87 @@
+// New Code CORRECT
+import java.util.*;
+class Node {
+    int data;
+    Node left, right;
+    Node(int _data) {
+        data = _data;
+        left = right = null;
+    }
+}
+public class BT {
+    static List<Integer> leftView(Node root) {
+        Node l = root.left;
+        ArrayList<Integer> left = new ArrayList<>();
+        while(l != null) {
+            if(l.left != null && l.right != null) left.add(l.data);
+            if(l.left == null) {
+                l = l.right;
+            } else {
+                l = l.left;
+            }
+        }
+        return left;
+    }
+    static void leaves(Node root, ArrayList<Integer> leaf) {
+        if(root == null) return;
+        if(root.left == null && root.right == null) leaf.add(root.data);
+        leaves(root.left, leaf);
+        leaves(root.right, leaf);
+    }
+    static List<Integer> rightView(Node root) {
+        Node r = root.right;
+        ArrayList<Integer> right = new ArrayList<>();
+        while(r!=null) {
+            if(r.left != null && r.right != null) right.add(r.data);
+            if(r.right == null) {
+                r = r.left;
+            } else {
+                r = r.right;
+            }
+        }
+        return right;
+    }
+    static List<Integer> boundary(Node root) {
+        List<Integer> left = leftView(root);
+        ArrayList<Integer> leaf = new ArrayList<>();
+        List<Integer> right =  rightView(root);
+        Collections.reverse(right);
+        leaves(root,leaf);
+        
+        System.out.println(left);
+        System.out.println(leaf);
+        System.out.println(right);
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(root.data);
+        result.addAll(left);
+        result.addAll(leaf);
+        result.addAll(right);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        // root.left = new Node(2);
+        root.right = new Node(2);
+        // root.left.left = new Node(4);
+        // root.left.right = new Node(5);
+        root.right.left = new Node(3);
+        root.right.right = new Node(4);
+        // Node root = new Node(1);
+        // root.left = new Node(2);
+        // root.right = new Node(3);
+        // root.left.left = new Node(4);
+        // root.left.right = new Node(5);
+        // root.right.left = new Node(6);
+        // root.right.right = new Node(7);
+        List<Integer> result = boundary(root);
+        for (int val : result) {
+            System.out.print(val + " ");
+        }
+    }
+}
+
+// Old Code WRONG
 class Solution {
     public ArrayList<Integer> rightSideView(Node root) {
         Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
