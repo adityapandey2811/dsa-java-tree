845b5da6c6128fc76724a4f4dd9c4b8f24961dfe
Aditya Pandey <2811adityapandey@gmail.com>
2025-02-22 23:21:51 +0530

    Create ConstructBSTfromPreorderTraversal.java

---
+++ ConstructBSTfromPreorderTraversal.java
@@ -0,0 +1,32 @@
+// TODO(Optimal Solution)
+
+class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        int []inorder = preorder.clone();
+        Arrays.sort(inorder);
+        return buildTree(preorder,inorder);
+    }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Stack<Integer> s = new Stack<>();
+        for(int i=preorder.length-1;i>=0;i--) {
+            s.push(preorder[i]);
+        }
+        return getTree(s, inorder);
+    }
+    public TreeNode getTree(Stack<Integer> s, int[] inorder) {
+        if(inorder.length > 0) {
+            TreeNode root = new TreeNode(s.pop());
+            root.left = getTree(s, Arrays.copyOfRange(inorder, 0, getIndex(root.val, inorder)));
+            root.right = getTree(s, Arrays.copyOfRange(inorder, 1+getIndex(root.val, inorder), inorder.length));
+            return root;
+        }
+        return null;
+    }
+    public int getIndex(int n, int[] inorder) {
+        int i;
+        for(i = 0;i<inorder.length;i++) {
+            if(inorder[i] == n) break;
+        }
+        return i;
+    }
+}
