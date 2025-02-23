05307fe890cd063fb1f8c6a7e868829d764231aa
Aditya Pandey <2811adityapandey@gmail.com>
2025-02-23 23:40:01 +0530

    Create ConstructBTfromPreorderAndPostorderTraversal.java

---
+++ ConstructBTfromPreorderAndPostorderTraversal.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    private int preIndex = 0;
+    private int postIndex = 0;
+
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        return constructTree(preorder, postorder);
+    }
+
+    private TreeNode constructTree(int[] preorder, int[] postorder) {
+        TreeNode root = new TreeNode(preorder[preIndex++]);
+        if (root.val != postorder[postIndex]) {
+            root.left = constructTree(preorder, postorder);
+        }
+        if (root.val != postorder[postIndex]) {
+            root.right = constructTree(preorder, postorder);
+        }
+        postIndex++;
+        return root;
+    }
+}
